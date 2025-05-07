56edc797835be4998a8b8e2adeb52ec83d722f0d
Rory OConnell <19547+RoryO@users.noreply.github.com>
2025-05-07 12:03:06 -0700

    The Extra only activates when in combat

--- Krod/Items/Tier2/TheExtra.cs
+++ Krod/Items/Tier2/TheExtra.cs
@@ -20,62 +20,69 @@ namespace Krod.Items.Tier2
 
             public void Update()
             {
-                extraStopwatch += Time.deltaTime;
-                if (extraStopwatch > 8f)
+                if (!body.outOfCombat)
+                {
+                    extraStopwatch += Time.deltaTime;
+                    if (extraStopwatch > 8f)
+                    {
+                        extraStopwatch = 0;
+                        if (!body || !body.teamComponent) { return; }
+                        int stacks = body.inventory.GetItemCount(KrodItems.TheExtra);
+
+                        // Similar to gas formula
+                        float damage = (1 + stacks) * body.damage * 0.75f;
+                        Log.Info($"d: {damage}");
+
+                        SphereSearch sphereSearch = new()
+                        {
+                            mask = LayerIndex.entityPrecise.mask,
+                            origin = body.transform.position,
+                            radius = 25 + (stacks * 5),
+                            queryTriggerInteraction = QueryTriggerInteraction.Collide
+                        };
+
+                        List<Collider> results = [];
+                        sphereSearch.RefreshCandidates()
+                            .FilterCandidatesByHurtBoxTeam(TeamMask.GetEnemyTeams(body.teamComponent.teamIndex))
+                            .RefreshCandidates()
+                            .GetColliders(results);
+                        int actual = 0;
+                        for (int i = 0; i < results.Count; ++i)
+                        {
+                            CharacterBody found = Util.HurtBoxColliderToBody(results[i]);
+                            GameObject gameObject = (found ? found.gameObject : null);
+                            if (!gameObject ||
+                                body.gameObject == gameObject ||
+                                found.teamComponent.teamIndex == body.teamComponent.teamIndex)
+                            {
+                                continue;
+                            }
+                            InflictDotInfo dotInfo = new()
+                            {
+                                attackerObject = body.gameObject,
+                                victimObject = gameObject,
+                                dotIndex = DotController.DotIndex.Burn,
+                                totalDamage = damage,
+                                maxStacksFromAttacker = (uint)stacks + 1,
+                                damageMultiplier = 1f,
+                            };
+                            StrengthenBurnUtils.CheckDotForUpgrade(body.inventory, ref dotInfo);
+                            DotController.InflictDot(ref dotInfo);
+                            actual++;
+                        }
+                        if (actual > 0)
+                        {
+                            EffectManager.SpawnEffect(blastSFX, new()
+                            {
+                                origin = body.transform.position,
+                                scale = 1f
+                            }, true);
+                        }
+                    }
+                }
+                else if (extraStopwatch > 0)
                 {
                     extraStopwatch = 0;
-                    if (!body || !body.teamComponent) { return; }
-                    int stacks = body.inventory.GetItemCount(KrodItems.TheExtra);
-
-                    // Similar to gas formula
-                    float damage = (1+stacks) * body.damage * 0.75f;
-                    Log.Info($"d: {damage}");
-
-                    SphereSearch sphereSearch = new()
-                    {
-                        mask = LayerIndex.entityPrecise.mask,
-                        origin = body.transform.position,
-                        radius = 25 + (stacks * 5),
-                        queryTriggerInteraction = QueryTriggerInteraction.Collide
-                    };
-
-                    List<Collider> results = [];
-                    sphereSearch.RefreshCandidates()
-                        .FilterCandidatesByHurtBoxTeam(TeamMask.GetEnemyTeams(body.teamComponent.teamIndex))
-                        .RefreshCandidates()
-                        .GetColliders(results);
-                    int actual = 0;
-                    for (int i = 0; i < results.Count; ++i)
-                    {
-                        CharacterBody found = Util.HurtBoxColliderToBody(results[i]);
-                        GameObject gameObject = (found ? found.gameObject : null);
-                        if (!gameObject || 
-                            body.gameObject == gameObject ||
-                            found.teamComponent.teamIndex == body.teamComponent.teamIndex) 
-                        { 
-                            continue; 
-                        }
-                        InflictDotInfo dotInfo = new()
-                        {
-                            attackerObject = body.gameObject,
-                            victimObject = gameObject,
-                            dotIndex = DotController.DotIndex.Burn,
-                            totalDamage = damage,
-                            maxStacksFromAttacker = (uint)stacks + 1,
-                            damageMultiplier = 1f,
-                        };
-                        StrengthenBurnUtils.CheckDotForUpgrade(body.inventory, ref dotInfo);
-                        DotController.InflictDot(ref dotInfo);
-                        actual++;
-                    }
-                    if (actual > 0)
-                    {
-                        EffectManager.SpawnEffect(blastSFX, new()
-                        {
-                            origin = body.transform.position,
-                            scale = 1f
-                        }, true);
-                    }
                 }
             }
         }
